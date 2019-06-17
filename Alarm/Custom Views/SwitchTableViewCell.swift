5a114d90cb2ea578cebf370410cad781d584155c
Darin Armstrong <darin.marmstrong@gmail.com>
2019-06-17 14:31:04 -0600

    Setup View and View Controller files

---
+++ Alarm/Custom Views/SwitchTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  SwitchTableViewCell.swift
+//  Alarm
+//
+//  Created by Darin Marcus Armstrong on 6/17/19.
+//  Copyright © 2019 Darin Marcus Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var alarmSwitch: UISwitch!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    @IBAction func switchValueChanged(_ sender: Any) {
+    }
+    
+}
